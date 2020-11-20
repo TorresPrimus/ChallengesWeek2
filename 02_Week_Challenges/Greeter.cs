2070fa6f70bda4c418f2eba45d208878146d85e8
Richard Torres <torresrichard252@gmail.com>
2020-11-19 19:17:40 -0500

    Initial Commit

---
+++ 02_Week_Challenges/Greeter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Week_Challenges
+{
+    class Greeter
+    {
+        public void HelloThere(string name)
+        {
+            Console.WriteLine("Hello there, " + name + ".");
+        }
+
+        public void ByeBye(string name)
+        {
+            Console.WriteLine("Adios " + name + ".");
+        }
+
+        public void TimeBased(string name)
+        {
+            if(DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("Good Morning " + name + ".");
+            }
+            
+            else if(DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 18)
+            {
+                Console.WriteLine("Good Afternoon " + name + ".");
+            }
+
+            else if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 24)
+            {
+                Console.WriteLine("Good Evening " + name + ".");
+            }
+
+            else 
+            {
+                Console.WriteLine("Good Night " + name + ".");
+            }
+        }
+    }
+}
---
+++ Week02_Challenges/Greeter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week02_Challenges
+{
+    public class Greeter
+    {
+        public void HelloThere(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+             
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello there, " + name);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
